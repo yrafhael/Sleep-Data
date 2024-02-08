319866417669300570d6035f84f24cc8cc563625
Yose Rafhael <yrafhael@my.wctc.edu>
2024-02-08 14:01:01 -0600

    [1.02] Create user options - Enter 1 to create data file - Enter 2 to parse data file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter 1 to create data file.");
+Console.WriteLine("Enter 2 to parse data.");
+Console.WriteLine("Enter anything else to quit.");
+// input response
+string? resp = Console.ReadLine();
+
+if (resp == "1")
+{
+    // TODO: create data file
+}
+else if (resp == "2")
+{
+    // TODO: parse data file
+}
