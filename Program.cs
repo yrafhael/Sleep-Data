4e96320a37c013a6d54edb12fff7851c9585bca5
Yose Rafhael <yrafhael@my.wctc.edu>
2024-02-08 14:19:25 -0600

    [1.06] Write data to text file

--- Program.cs
+++ Program.cs
@@ -6,23 +6,24 @@ string? resp = Console.ReadLine();
 
 if (resp == "1")
 {
-      // create data file
+    // create data file
 
     // ask a question
     Console.WriteLine("How many weeks of data is needed?");
     // input the response (convert to int)
     int weeks = int.Parse(Console.ReadLine());
 
-        // determine start and end date
+    // determine start and end date
     DateTime today = DateTime.Now;
     // we want full weeks sunday - saturday
     DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
     // subtract # of weeks from endDate to get startDate
     DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
-    
+
     Console.WriteLine(dataDate);
-        // random number generator
+    // random number generator
     Random rnd = new Random();
+     StreamWriter sw = new StreamWriter("data.txt");
 
     // loop for the desired # of weeks
     while (dataDate < dataEndDate)
@@ -35,10 +36,12 @@ if (resp == "1")
             hours[i] = rnd.Next(4, 13);
         }
         // M/d/yyyy,#|#|#|#|#|#|#
-        Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+        // Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+        sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
         // add 1 week to date
         dataDate = dataDate.AddDays(7);
     }
+    sw.Close();
 }
 else if (resp == "2")
 {
